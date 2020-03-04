70da90be4dac968bfcffe96fdd9689e19aa77e0d
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 01:45:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,7 @@ def total_gross(source)
   answer = 0 
   d_i = 0 
     while d_i < totals.length do 
-      answer += totals[d_i]
+      answer += totals[d_i][:worldwide_gross]
       d_i += 1 
     end 
     answer
