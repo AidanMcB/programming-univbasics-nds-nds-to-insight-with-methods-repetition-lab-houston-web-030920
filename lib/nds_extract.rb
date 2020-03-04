013193bb945cae818fafd549794915a670775fe4
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 01:41:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,7 +43,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+  director_total = 
   
 
 end
